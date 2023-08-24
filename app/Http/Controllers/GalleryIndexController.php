f24bac31ecc76295a93d0f518b511bd1fa9072d5
samandar <xkas2001@gmail.com>
2023-08-24 11:21:14 +0500

    Created Event and Gallery Index

---
+++ app/Http/Controllers/GalleryIndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gallery;
+use Illuminate\Http\Request;
+
+class GalleryIndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke()
+    {
+        $galleries = Gallery::orderBy('created_at', 'desc')->paginate(12);
+        return view('galleryIndex', compact('galleries'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\AttendingSystemController;
 use App\Http\Controllers\DeleteCommentController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\EventIndexController;
 use App\Http\Controllers\EventShowController;
 use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\GalleryIndexController;
 use App\Http\Controllers\LikeSystemController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SaveSystemController;
@@ -25,7 +27,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', WelcomeController::class)->name('welcome');
+Route::get('/e', EventIndexController::class)->name('eventIndex');
 Route::get('/e/{id}', EventShowController::class)->name('eventShow');
+Route::get('/gallery', GalleryIndexController::class)->name('galleryIndex');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
